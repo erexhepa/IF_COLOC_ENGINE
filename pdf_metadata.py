65a19af38a5f76fe4fafb480a6c6b2314af96b91
erexhepa <elton.rexhepaj@gmail.com>
2015-11-04 12:27:05 +0100

    First version of the IF histogram search engine

--- pdf_metadata.py
+++ pdf_metadata.py
@@ -1,12 +1,29 @@
 import pyPdf
 import optparse
 from pyPdf import PdfFileReader
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+
+def get_toc(pdf_path):
+    infile = open(pdf_path, 'rb')
+    parser = PDFParser(infile)
+    document = PDFDocument(parser)
+
+    toc = list()
+    for (level,title,dest,a,structelem) in document.get_outlines():
+        toc.append((level, title))
+
+    return toc
 
 def printMeta(fileName):
     pdfFile = PdfFileReader(file(fileName,'rb'))
     docInfo = pdfFile.getDocumentInfo()
+    pginput = pdfFile.getPage(1)
 
+    print "title = %s" % (pdfFile.getDocumentInfo().title)
     print '[*] PDF MetaData For: '+ str(fileName)
+
+    tocfilepdf = get_toc(fileName)
 
     for metaItem in docInfo:
         print '[+]' + metaItem + ':' + docInfo[metaItem]
