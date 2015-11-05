ec3b93d2da17e1944de1ad3bea7d8064d96e3284
erexhepa <elton.rexhepaj@gmail.com>
2015-11-05 13:25:22 +0100

    First version of the IF histogram search engine

--- pdf_metadata.py
+++ pdf_metadata.py
@@ -4,6 +4,36 @@ from pyPdf import PdfFileReader
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 
+import zipfile
+from lxml import etree
+
+def get_epub_info(fname):
+    ns = {
+        'n':'urn:oasis:names:tc:opendocument:xmlns:container',
+        'pkg':'http://www.idpf.org/2007/opf',
+        'dc':'http://purl.org/dc/elements/1.1/'
+    }
+
+    # prepare to read from the .epub file
+    zip = zipfile.ZipFile(fname)
+
+    # find the contents metafile
+    txt = zip.read('META-INF/container.xml')
+    tree = etree.fromstring(txt)
+    cfname = tree.xpath('n:rootfiles/n:rootfile/@full-path',namespaces=ns)[0]
+
+    # grab the metadata block from the contents metafile
+    cf = zip.read(cfname)
+    tree = etree.fromstring(cf)
+    p = tree.xpath('/pkg:package/pkg:metadata',namespaces=ns)[0]
+
+    # repackage the data
+    res = {}
+    for s in ['title','language','creator','date','identifier']:
+        res[s] = p.xpath('dc:%s/text()'%(s),namespaces=ns)[0]
+
+    return res
+
 def get_toc(pdf_path):
     infile = open(pdf_path, 'rb')
     parser = PDFParser(infile)
@@ -35,6 +65,8 @@ def main():
 
     (options, args) = parser.parse_args()
     fileName = options.fileName
+
+    #fmeta = get_epub_info('/home/eltonr/Downloads/'+fileName)
 
     if fileName == None:
         print parser.usage
